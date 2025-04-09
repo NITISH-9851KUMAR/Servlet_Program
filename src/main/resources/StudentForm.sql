8062c6cc1535c9f156e891704b9d103bf39bb534
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-04-09 16:26:38 +0530

    Table Store Student Details

---
+++ src/main/resources/StudentForm.sql
@@ -0,0 +1,7 @@
+CREATE TABLE student_form( /* Table for Store Student Details */
+                name VARCHAR(50),
+                email VARCHAR(30),
+                phone VARCHAR(13),
+                gender VARCHAR(7),
+                branch VARCHAR(5),
+                dob VARCHAR(12));
