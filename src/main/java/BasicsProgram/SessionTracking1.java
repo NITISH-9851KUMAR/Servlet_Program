b8fa8d4d8eeb7b2c5c7166a4717d171734a94d3f
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-05-13 00:00:35 +0530

    Session Tracking Servlets Class

---
+++ src/main/java/BasicsProgram/SessionTracking1.java
@@ -0,0 +1,32 @@
+package BasicsProgram;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+
+
+@WebServlet("/servlet1")
+public class SessionTracking1 extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException{
+
+        response.setContentType("text/html");
+        PrintWriter out= response.getWriter();
+
+        String name= request.getParameter("Name");
+
+        out.println("<h1>Name: "+name+"</h1>");
+        out.println("<h1>Welcome to Servlet One</h1>");
+        out.println("<h1> <a href='servlet2'>Go to Servlet2</a>\n</h1>");
+
+        Cookie c= new Cookie("user_name", name);
+        response.addCookie(c);
+
+    }
+}
