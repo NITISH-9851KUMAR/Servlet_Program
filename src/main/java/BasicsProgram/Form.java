f3e504b3bdfee85569122cd925336efffe995805
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-05-14 21:35:46 +0530

    Add field to take photo

--- src/main/java/BasicsProgram/Form.java
+++ src/main/java/BasicsProgram/Form.java
@@ -1,18 +1,20 @@
 package BasicsProgram;
 
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.RequestDispatcher;
-import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.http.Part;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 @WebServlet("/Form")
+@MultipartConfig
 
 public class Form extends HttpServlet {
 
@@ -29,6 +31,9 @@ public class Form extends HttpServlet {
         String name= request.getParameter("user_name");
         String email= request.getParameter("email");
         String password= request.getParameter("password");
+        Part part= request.getPart("image"); // Part is type of file store photo
+        String fileName= part.getSubmittedFileName();
+        out.println(fileName);
 
         // JDBC Program
 
@@ -38,14 +43,23 @@ public class Form extends HttpServlet {
 
             Connection connection= DriverManager.getConnection(url, userName, pass);
 
-            String query= "INSERT INTO form VALUES(?, ?, ?)";
+            String query= "INSERT INTO form VALUES(?, ?, ?, ?)";
             PreparedStatement pStmt= connection.prepareStatement(query);
             pStmt.setString(1, name);
             pStmt.setString(2, email);
             pStmt.setString(3, password);
+            pStmt.setString(4, fileName);
 
             pStmt.executeUpdate();
-
+            // file upload
+            InputStream is= part.getInputStream();
+            byte[] data= new byte[is.available()];
+            is.read(data);
+            String path= request.getRealPath("/")+"image"+ File.separator+fileName;
+            out.println(path);
+            FileOutputStream fos= new FileOutputStream(path);
+            fos.write(data);
+            fos.close();
             out.println("done");
 //            System.out.println("Data Successfully save to the Database");
 ////
